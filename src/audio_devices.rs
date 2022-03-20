ce583d34f32bbfe79fa9c2e560c68b47faef004a
antisvin <antisvin@gmail.com>
2022-03-20 23:13:42 +0300

    Sine wave output works

--- src/audio_devices.rs
+++ src/audio_devices.rs
@@ -105,9 +105,9 @@ impl AudioHandler {
                  */
 
                 self.output_stream = match sample_format {
-                    cpal::SampleFormat::F32 => Some(run::<f32>(device, &config)),
-                    cpal::SampleFormat::I16 => Some(run::<i16>(device, &config)),
-                    cpal::SampleFormat::U16 => Some(run::<u16>(device, &config)),
+                    cpal::SampleFormat::F32 => Some(self.run::<f32>(device, &config)),
+                    cpal::SampleFormat::I16 => Some(self.run::<i16>(device, &config)),
+                    cpal::SampleFormat::U16 => Some(self.run::<u16>(device, &config)),
                 };
                 self.output_stream.as_ref().unwrap().play()?;
                 /*
@@ -130,6 +130,33 @@ impl AudioHandler {
     }
 
     pub fn select_input(&mut self, _host_id: Option<HostId>, _device: Option<usize>) {}
+
+    fn run<T>(&self, device: &cpal::Device, config: &cpal::StreamConfig) -> Stream
+    where
+        T: cpal::Sample,
+    {
+        let sample_rate = config.sample_rate.0 as f32;
+        let channels = config.channels as usize;
+
+        // Produce a sinusoid of maximum amplitude.
+        let mut sample_clock = 0f32;
+        let mut next_value = move || {
+            sample_clock = (sample_clock + 1.0) % sample_rate;
+            (sample_clock * 440.0 * 2.0 * std::f32::consts::PI / sample_rate).sin()
+        };
+
+        let err_fn = |err| print!("an error occurred on stream: {}", err);
+
+        let stream = device
+            .build_output_stream(
+                config,
+                move |data: &mut [T], _| write_data(data, channels, &mut next_value),
+                err_fn,
+            )
+            .unwrap();
+        stream.play().unwrap();
+        stream
+    }
 }
 /*
 fn write_data<T>(output: &mut [T], channels: usize, next_sample: &mut dyn FnMut() -> f32)
@@ -173,32 +200,6 @@ pub fn init_audio() -> Handle {
     })
 }
 */
-fn run<T>(device: &cpal::Device, config: &cpal::StreamConfig) -> Stream
-where
-    T: cpal::Sample,
-{
-    let sample_rate = config.sample_rate.0 as f32;
-    let channels = config.channels as usize;
-
-    // Produce a sinusoid of maximum amplitude.
-    let mut sample_clock = 0f32;
-    let mut next_value = move || {
-        sample_clock = (sample_clock + 1.0) % sample_rate;
-        (sample_clock * 440.0 * 2.0 * std::f32::consts::PI / sample_rate).sin()
-    };
-
-    let err_fn = |err| print!("an error occurred on stream: {}", err);
-
-    let stream = device
-        .build_output_stream(
-            config,
-            move |data: &mut [T], _| write_data(data, channels, &mut next_value),
-            err_fn,
-        )
-        .unwrap();
-    stream.play().unwrap();
-    stream
-}
 
 fn write_data<T>(output: &mut [T], channels: usize, next_sample: &mut dyn FnMut() -> f32)
 where
